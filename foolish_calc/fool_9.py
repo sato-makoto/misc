41425fbc81e8d84d76e359a8c5e67a2ba0e9cfc1
Sato Makoto <makoto@kuzuore.com>
2015-07-27 13:48:30 +0900

    change to check numbers function

--- foolish_calc/fool_9.py
+++ foolish_calc/fool_9.py
@@ -1,9 +1,19 @@
 def foolish(a,b,c,d,e,f,g,h,i):
-  myset = set([a,b,c,d,e,f,g,h,i])
-  if len(myset) == 9:
-    return True
+  if a == b or a == c or a == d or a == e or \
+   a == f or a == g or a == h or a == i or \
+   b == c or b == d or b == e  or \
+   b == f or b == g or b == h or b == i or \
+   c == d or c == e  or \
+   c == f or c == g or c == h or c == i or \
+   d == e  or \
+   d == f or d == g or d == h or d == i or \
+   e == f or e == g or e == h or e == i or \
+   f == g or f == h or f == i or \
+   g == h or g == i or \
+   h == i :
+     return False
   else:
-    return False
+     return True
 
 lines = [ x for x in range(1,10)]
 
@@ -16,8 +26,8 @@ for k in lines:
             for q in lines:
               for r in lines:
                  for s in lines:
-                   if foolish(k, l, m, n, o, p, q, r, s):
-#                      if (13.0 * l) % m  == 0 and (q * r) % s == 0 \
-#                       and k + 13 * l / m + n + 12 * o - p - 11 + q * r / s - 10 == 66:
+                   if foolish(k, l, m, n, o, p, q, r, s) and \
+                       (13.0 * l) % m  == 0 and (q * r) % s == 0 \
+                       and k + 13 * l / m + n + 12 * o - p - 11 + q * r / s - 10 == 66:
                         print k, l, m, n, o, p, q, r, s
 
