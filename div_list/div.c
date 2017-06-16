46305310268738a080ca12ac47480ba0e2e4bcfc
Sato Makoto <makoto@kuzuore.com>
2017-06-16 20:44:35 +0900

    diet main()

--- div_list/div.c
+++ div_list/div.c
@@ -1,30 +1,37 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main(int argc, char **argv){
+void mklist();
 
-  int num = atoi(argv[1]);
-
+void mklist(int num) {
   int mylist[4] = {0,0,0,0};
-   if(num < 100) {
-     mylist[3] = num;
-   }else if(num < 10000) {
-     mylist[3] = num % 100;
+  if(num < 100) {
+     mylist[3] = num; 
+  }else if(num < 10000) {
      mylist[2] = num / 100;
-   }else if(num < 1000000) {
-     mylist[3] = num % 10000 % 100;
-     mylist[2] = num % 10000 / 100;
+     mylist[3] = num % 100;
+  }else if(num < 1000000) {
      mylist[1] = num / 10000;
-   }else if(num < 1000000) {
-   }else {
-     mylist[3] = num % 1000000 % 10000 % 100;
-     mylist[2] = num % 1000000 % 10000 / 100;
-     mylist[1] = num % 1000000 / 10000 ;
-     mylist[0] = num / 1000000;
-     }
+     mylist[2] = num % 10000 / 100;
+     mylist[3] = num % 10000 % 100;
+  }else {
+  mylist[3] = num % 1000000 % 10000 % 100;
+  mylist[2] = num % 1000000 % 10000 / 100;
+  mylist[1] = num % 1000000 / 10000 ;
+  mylist[0] = num / 1000000;
+  }
   for(int i=0; i<4; i++) {
     printf("%d,", mylist[i]);
   }
   printf("\n");
-  return 0;
+}
+
+int main(int argc, char **argv){
+  int num = atoi(argv[1]);
+  if(num > 99999999) {
+    printf("Too big!\n");
+    exit(1);
+  }
+  mklist(num);
+  return 0; 
 }
