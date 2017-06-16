9d332ed546490f2cbd3dae10d523609389fc6791
Sato Makoto <makoto@kuzuore.com>
2017-06-16 20:55:19 +0900

    change s/100/HUND/, etc

--- div_list/div.c
+++ div_list/div.c
@@ -1,24 +1,28 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#define HUND 100
+#define HUND2 10000
+#define HUND3 1000000
+
 void mklist();
 
 void mklist(int num) {
   int mylist[4] = {0,0,0,0};
-  if(num < 100) {
+  if(num < HUND) {
      mylist[3] = num; 
-  }else if(num < 10000) {
-     mylist[2] = num / 100;
-     mylist[3] = num % 100;
-  }else if(num < 1000000) {
-     mylist[1] = num / 10000;
-     mylist[2] = num % 10000 / 100;
-     mylist[3] = num % 10000 % 100;
+  }else if(num < HUND2) {
+     mylist[2] = num / HUND;
+     mylist[3] = num % HUND;
+  }else if(num < HUND3) {
+     mylist[1] = num / HUND2;
+     mylist[2] = num % HUND2 / HUND;
+     mylist[3] = num % HUND2 % HUND;
   }else {
-  mylist[3] = num % 1000000 % 10000 % 100;
-  mylist[2] = num % 1000000 % 10000 / 100;
-  mylist[1] = num % 1000000 / 10000 ;
-  mylist[0] = num / 1000000;
+  mylist[0] = num / HUND3;
+  mylist[1] = num % HUND3 / HUND2 ;
+  mylist[2] = num % HUND3 % HUND2 / HUND;
+  mylist[3] = num % HUND3 % HUND2 % HUND;
   }
   for(int i=0; i<4; i++) {
     printf("%d,", mylist[i]);
