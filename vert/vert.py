84c4ff9017f6bb4fd54e755ad869a12e43a65814
Sato Makoto <makoto@kuzuore.com>
2017-10-01 04:37:55 +0900

    add max_char

--- vert/vert.py
+++ vert/vert.py
@@ -1,24 +1,33 @@
 #!/usr/bin/env python3
 from sys import argv
 
+max_char = 13
 f = open(argv[1],'r')
-list = []
+list1 = []
 for x in f:
-  list.append(x.strip())
+  list1.append(x.strip())
 f.close()
 
 print('テストです',end='\n\n') 
 
-for y in list:
+for y in list1:
   for z in y:
     print(z, end=' ')
   print(' ')
 
 print('')
 
-list3 =[]
-for x in range(9):
-  bar = [foo[x] for foo in list]
+list2 = []
+for x in list1:
+  list2.append(list(x))
+
+for y in list2:
+  while len(y) < max_char + 1:
+    y.append('＿')
+
+list3 = []
+for x in range(max_char):
+  bar = [foo[x] for foo in list2]
   bar.reverse()
   list3.append(bar)
 
