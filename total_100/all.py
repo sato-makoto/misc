7b69b81dc751f271cd647f7442f5e85c78a7f729
Sato Makoto <makoto@kuzuore.com>
2018-04-16 00:34:34 +0900

    add 65536 anser

---
+++ total_100/all.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+four_op="+-*/"
+
+for a in four_op:
+  for b in four_op:
+    for c in four_op:
+      for d in four_op:
+        for e in four_op:
+          for f in four_op:
+            for g in four_op:
+              for h in four_op:
+                num = eval(f"1 {a!s} 2 {b!s} 3 {c!s} 4 {d!s} 5 {e!s} \
+                             6 {f!s} 7 {g!s} 8 {h!s} 9")
+                print (f"1 {a!s} 2 {b!s} 3 {c!s} 4 {d!s} 5 {e!s} \
+                           6 {f!s} 7 {g!s} 8 {h!s} 9 = {num!s}".replace("   ",""))
