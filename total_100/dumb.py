6138c8ac1ae0adfc47a3f5dce6c72b6505a5f2c4
Sato Makoto <makoto@kuzuore.com>
2018-04-20 11:21:11 +0900

    add argv and handling exception

--- total_100/dumb.py
+++ total_100/dumb.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
+from sys import argv
 
 four_op="+-*/"
+
+try:
+  [one, two, three, four, five, six, seven, eight, nine ] = \
+    argv[1], argv[2], argv[3], argv[4], argv[5], argv[6], \
+    argv[7], argv[8], argv[9]
+except:
+  [one, two, three, four, five, six, seven, eight, nine ] = \
+    [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 for a in four_op:
   for b in four_op:
@@ -10,8 +19,10 @@ for a in four_op:
           for f in four_op:
             for g in four_op:
               for h in four_op:
-                num = eval('1 {} 2 {} 3 {} 4 {} 5 {} 6 {} 7 {} 8 {} 9'. \
-                    format(a, b, c, d, e, f, g, h))
+                num = eval('{} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {}'. \
+                    format(one, a, two, b, three, c, four, d, five, e, six, f, \
+                        seven, g, eight, h, nine))
                 if num == 100:
-                    print('1 {} 2 {} 3 {} 4 {} 5 {} 6 {} 7 {} 8 {} 9 = {}'. \
-                       format(a, b, c, d, e, f, g, h, num))
+                    print('{} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} = {}'. \
+                    format(one, a, two, b, three, c, four, d, five, e, six, f, \
+                        seven, g, eight, h, nine, num))
