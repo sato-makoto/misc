f2a4e15ae3fe451d0b565bdcd56b03c437031c5e
Sato Makoto <makoto@kuzuore.com>
2018-04-23 15:01:09 +0900

    copy from my blog 2012-12-07

---
+++ lua/fizzbuzz_100.lua
@@ -0,0 +1,13 @@
+for x=1, 100, 1
+do  
+  if(x%15<1)
+    then print('fizzbuzz')
+  elseif(x%3<1)
+    then print('fizz')
+  elseif(x%5<1)
+    then print("buzz")
+  else
+    print(x)
+  end
+end
+
