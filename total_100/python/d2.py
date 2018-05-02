18b18c025df63dc8bf949d46a9849f3482962bb2
Sato Makoto <makoto@kuzuore.com>
2018-05-02 17:25:39 +0900

    remove format()

--- total_100/python/d2.py
+++ total_100/python/d2.py
@@ -3,6 +3,7 @@ from sys import argv
 
 four_op="+-*/"
 full_num = 4**8
+sp = ' '
 
 try:
   [one, two, three, four, five, six, seven, eight, nine ] = \
@@ -10,20 +11,18 @@ try:
     argv[7], argv[8], argv[9]
 except:
   [one, two, three, four, five, six, seven, eight, nine ] = \
-    [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 for num in range(full_num):
-	total = '{} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {}'. \
-		format(
-		one, four_op[num//16384%4],
-		two, four_op[num//4096%4],
-		three, four_op[num//1024%4],
-		four, four_op[num//256%4],
-		five, four_op[num//63%4],
-		six, four_op[num//16%4],
-		seven, four_op[num//4%4],
-		eight, four_op[num%4],
-		nine)
+	total = one + sp + four_op[num//16384%4] + sp + \
+		two + sp + four_op[num//4096%4] + sp + \
+		three + sp + four_op[num//1024%4] + sp + \
+		four + sp + four_op[num//256%4] + sp + \
+		five + sp + four_op[num//63%4] + sp + \
+		six + sp + four_op[num//16%4] + sp + \
+		seven + sp + four_op[num//4%4] + sp + \
+		eight + sp + four_op[num%4] + sp + \
+		nine
 	evnum = eval(total)
 	if evnum == 100:
 		print(total, ' = ', evnum)
