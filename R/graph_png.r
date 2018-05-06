d17e01aeb01f70e65503847d7ded517a4ab6b57e
Sato Makoto <makoto@kuzuore.com>
2018-05-06 20:28:26 +0900

    add make graph script of R

---
+++ R/graph_png.r
@@ -0,0 +1,17 @@
+#!/usr/bin/env Rscript
+
+# $ for x in `seq 10000`; do echo `expr $RANDOM % 83` >> times.txt; done
+# $ sort -n times.txt |uniq -c | awk '{print $2, $1}' >source.txt
+
+ 
+mydata=read.table("source.txt", header=T)
+png(filename="bash_random.png", width=800,height=600)
+plot    (
+        mydata$Number, mydata$Times,
+        type="l", 
+        xlab="Number",
+        ylab="Times",
+        main="10000times $RANDOM % 83",
+        )
+dev.off()
+
