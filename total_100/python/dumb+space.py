b86eb86cde76a0a18a835b91f6aa27121d8d943d
Sato Makoto <makoto@kuzuore.com>
2018-05-19 08:19:40 +0900

    add space to four operators to change numbers like "1 2 3" to "12 + 3"

---
+++ total_100/python/dumb+space.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+from sys import argv
+
+fifth_op="+-*/ "
+
+try:
+  [one, two, three, four, five, six, seven, eight, nine ] = \
+    argv[1], argv[2], argv[3], argv[4], argv[5], argv[6], \
+    argv[7], argv[8], argv[9]
+except:
+  [one, two, three, four, five, six, seven, eight, nine ] = \
+    [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for a in fifth_op:
+  for b in fifth_op:
+    for c in fifth_op:
+      for d in fifth_op:
+        for e in fifth_op:
+          for f in fifth_op:
+            for g in fifth_op:
+              for h in fifth_op:
+                num='{} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {}'.\
+                    format(one, a, two, b, three, c, four, d, five, e, six, f, \
+                        seven, g, eight, h, nine)
+                num = num.replace(' ', '')
+                if eval(num) == 100:
+                    print(num, '=', eval(num))
