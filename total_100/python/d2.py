ffe35a6f032f0ca197bf2cb294a60cadca2ec78c
Sato Makoto <makoto@kuzuore.com>
2018-05-26 23:15:58 +0900

    change 2^n to 4^n/2

--- total_100/python/d2.py
+++ total_100/python/d2.py
@@ -16,14 +16,14 @@ except:
     all = 100
 
 for num in range(full_num):
-	total = one + sp + four_op[num//2**14%4] + sp + \
-		two + sp + four_op[num//2**12%4] + sp + \
-		three + sp + four_op[num//2**10%4] + sp + \
-		four + sp + four_op[num//2**8%4] + sp + \
-		five + sp + four_op[num//2**6%4] + sp + \
-		six + sp + four_op[num//2**4%4] + sp + \
-		seven + sp + four_op[num//2**2%4] + sp + \
-		eight + sp + four_op[num//2**0%4] + sp + \
+	total = one + sp + four_op[num//4**7%4] + sp + \
+		two + sp + four_op[num//4**6%4] + sp + \
+		three + sp + four_op[num//4**5%4] + sp + \
+		four + sp + four_op[num//4**4%4] + sp + \
+		five + sp + four_op[num//4**3%4] + sp + \
+		six + sp + four_op[num//4**2%4] + sp + \
+		seven + sp + four_op[num//4**1%4] + sp + \
+		eight + sp + four_op[num//4**0%4] + sp + \
 		nine
 	evnum = eval(total)
 	if evnum == all:
