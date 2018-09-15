7395c0f5d84b1e40fb95f48a167c04a654a7f093
Sato Makoto <makoto@kuzuore.com>
2018-09-16 00:09:27 +0900

    changed from set to list

--- python3/three_money.py
+++ python3/three_money.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
-mset = set()
+setlist = []
 mlist = [1,5,10,50,100,500]
 for x in mlist:
     for y in mlist:
         for z in mlist:
             t = x+y+z
-            if not t in mset:
-                mset.add(t)
+            if not t in setlist:
+                setlist.append(t)
                 print(x,"+",y,"+",z,"=",x+y+z)
 
-# print(mset)
+# setlist.sort()
+# print(setlist)
