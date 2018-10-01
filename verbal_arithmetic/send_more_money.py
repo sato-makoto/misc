13b1a839f7d7132b16d9554f093637a881f7d7f4
Sato Makoto <makoto@kuzuore.com>
2018-10-01 22:06:38 +0900

    change faster

--- verbal_arithmetic/send_more_money.py
+++ verbal_arithmetic/send_more_money.py
@@ -8,11 +8,11 @@
 S E N D M O R Y
 """
 
-for s in range(1,10):
+for m in range(1,10):
   for e in range(10):
     for n in range(10):
       for d in range(10):
-        for m in range(1,10):
+        for s in range(1,10):
           for o in range(10):
             for r in range(10):
               for y in range(10):
@@ -22,3 +22,4 @@ for s in range(1,10):
                     len({s, e, n, d, m, o, r, y}) == 8:
                   print(s, e, n, d, "+", m, o, r, e, "=", m, o, n, e, y )
                   print('s e n d + m o r e = m o n e y' )
+                  exit(0)
