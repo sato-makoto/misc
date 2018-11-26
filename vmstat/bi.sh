1c486ef988e4a6a44c9dd29055da00b4ad5611f3
Sato Makoto <makoto@kuzuore.com>
2018-11-26 17:52:59 +0900

    change from head 100 to 300..400 lines

--- vmstat/bi.sh
+++ vmstat/bi.sh
@@ -5,7 +5,7 @@
 # awk rounding technique is from 
 # https://blog.trippyboy.com/2011/shell/bash%E3%81%A7%E5%9B%9B%E6%8D%A8%E4%BA%94%E5%85%A5%E3%81%97%E3%81%9F%E3%81%84%E3%81%AE%E5%B7%BB/
 
-echo "Time BS" >d
-grep "^ [0-9]" typescript |sed -n 300,400p| \
-	awk '{printf(NR" "); printf("%d\n", $10/1024+0.5)}' >>d
-Rscript vmstat_png.r d
+echo "Time BS" >i
+grep "^ [0-9]" typescript | sed -n 300,400p | \
+	awk '{printf(NR" "); printf("%d\n", $9/1024+0.5)}' >>i
+Rscript vmstat_png.r i
