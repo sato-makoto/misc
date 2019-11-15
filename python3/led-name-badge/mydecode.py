bc02992542ea837e0440cd2a641faabacc2f39a4
Sato Makoto <makoto@kuzuore.com>
2019-11-15 16:27:26 +0900

    some improvement

--- python3/led-name-badge/mydecode.py
+++ python3/led-name-badge/mydecode.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 # led-name-badge-ls32 font decode test script 
-import re
-mystr = [0x00, 0x00, 0x00, 0x18, 0x18, 0x7e, 0x18, 0x18, 0x00, 0x00, 0x00]
-for x in mystr:
-    decode = '{:>10}'.format(bin(x)).replace('0b','').replace(' ','0')
-    myline = re.sub('(.)', r'\1 ', decode)
-    print(myline.replace("1", "\33[31m1\033[00m"))
 
+# arg(1) means strings like "0x00,0x38,0x6c,0xc6,0xc6,0xfe,0xc6,0xc6,0xc6,0xc6,0x00"
+# with NO space
+
+import re
+from sys import argv
+
+esc_num=36
+esc = "\33[{}m0\033[00m".format(esc_num)
+mystr = argv[1].split(',')
+# print (mystr)
+
+for x in mystr:
+    x = int(x,16)
+    decode = '{:>10}'.format(bin(x)).replace('0b','').replace(' ','0')
+    myline = re.sub('(.)', r'\1 ', decode).replace('0',' ')
+    print(myline.replace("1", esc))
