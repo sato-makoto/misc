3363b6dcee03ff66770c4d4b2de43ea0970d2045
Sato Makoto <makoto@kuzuore.com>
2020-04-25 14:25:09 +0900

    t = 1

--- send_more_money/python/six_seven_seven.py
+++ send_more_money/python/six_seven_seven.py
@@ -4,19 +4,19 @@
 # s i x e v n t w y
 # (s, 
 
+t = 1
 for y in range(10):
     for w in range(10):
-        for t in range(1,10):
-            for n in range(10):
-                for v in range(10):
-                    for e in range(10):
-                        for x in range(10):
-                            for i in range(10):
-                                for s in range(1,10):
-                                    check = {s, i, x, e, v, n, t, w, y}
-                                    six = s * 100 + i * 10 + x
-                                    seven = s * 10000 + e * 1000 + v * 100 + e * 10 + n
-                                    twenty = t * 100000 + w * 10000 + e * 1000 + n * 100 + t * 10 + y
-                                    if (len(check) == 9) and  \
-                                            six + seven * 2 == twenty:
-                                                print(s, i, x, "+", s, e, v, e, n, "+", s, e, v, e, n, "=", t, w, e, n, t, y)
+        for n in range(10):
+            for v in range(10):
+                for e in range(10):
+                    for x in range(10):
+                        for i in range(10):
+                            for s in range(1,10):
+                                check = {s, i, x, e, v, n, t, w, y}
+                                six = s * 100 + i * 10 + x
+                                seven = s * 10000 + e * 1000 + v * 100 + e * 10 + n
+                                twenty = t * 100000 + w * 10000 + e * 1000 + n * 100 + t * 10 + y
+                                if (len(check) == 9) and  \
+                                        six + seven * 2 == twenty:
+                                            print(s, i, x, "+", s, e, v, e, n, "+", s, e, v, e, n, "=", t, w, e, n, t, y)
