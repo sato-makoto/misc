7f9cbd90558d98d2998f9e334236752df38dc39f
Sato Makoto <makoto@kuzuore.com>
2020-04-25 21:42:13 +0900

    remove coment

--- verbal_arithmetic/s2.1.py
+++ verbal_arithmetic/s2.1.py
@@ -3,12 +3,6 @@
 # send + more = money
 # s e n d m o r y
 #  [0 - s] [1 - e] [2 - n] [3 - d] [4 - m] [5 - o] [6 - r] [7 - y]
-"""
-                            if (len(check) == 8) and  \
-                                    (s * 1000 + e * 100 + n * 10 + d 
-                                            + m * 1000 + o * 100 + r * 10 + e
-                                            == m * 10000 + o * 1000 + n * 100 + e * 10 + y):
-"""
 m = 1
 for y in range(10):
     for r in range(10):
