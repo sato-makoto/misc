1f3603cb575431a46b2140f3a5eabae299d94718
Sato Makoto <makoto@kuzuore.com>
2020-05-29 14:49:39 +0900

    reduce loop times bat not yet

--- missing_figure/7th.py
+++ missing_figure/7th.py
@@ -9,9 +9,13 @@
 a / b = *7***
 
 """
+for c in range(10000,100000):
+  if c // 1000 % 10 == 7:
+    for a in range(10000000,1000000000):
+      if a % c == 0:
+        for b in range(100,1000):
+          if c == a / b and a == b * c and a // 100000 < b:
+            print(a, " / ", b, " = ", c)
+#            exit(0)
 
-for a in range(100,1000):
-  for b in range(10000000,1000000000):
-    for c in range(10000,100000):
-       if c // 1000 % 10 == 7 and c == a / b:
-          print(a, " / ", b, " = ", c)
+
