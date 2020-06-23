020505a3ee87d05187bef70d949a31ffbbd0d37c
Sato Makoto <makoto@kuzuore.com>
2020-06-23 17:48:13 +0900

    add await

---
+++ R/iostat_await.r
@@ -0,0 +1,23 @@
+#!/usr/bin/env Rscript
+
+r_await=read.table("sda_r_await", header=T)
+w_await=read.table("sdb_w_await", header=T)
+png(filename="sda_r_sdb_w_await.png", width=1024,height=768)
+#pdf(file="test.pdf")
+plot(
+	w_await$Time, w_await$w_await, type="l", col="3",
+        xlab="時間(秒)",
+        ylab="await(ms)",
+#	ylim=c(-5,105),
+        main="ddrescue iostat"
+)
+points(
+	r_await$Time, r_await$r_await, type="l", col="2",
+)
+legend("topright",c("sda: r_await", "sdb: w_awiat"),
+		pch = 15,
+		col = c(2,3)
+	)
+warnings()
+dev.off()
+
