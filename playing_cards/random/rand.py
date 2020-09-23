8311986962eb223214d1873d2e5db816294c0c93
Sato Makoto <makoto@kuzuore.com>
2020-09-23 17:36:59 +0900

    add members parameter

--- playing_cards/random/rand.py
+++ playing_cards/random/rand.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
 import random
+from sys import argv
+"""
+カードを member 人に配ったらどんな札が来るか
+"""
+
+if len(argv) == 1:
+  members = 3
+else:
+  members = int(argv[1])
 
 hearts = [(x, "ハート") for x in range(1,14)]
 tiles = [(x, "ダイヤ") for x in range(1,14)]
@@ -14,7 +23,7 @@ for x in all:
 """
 
 mycards = []
-for x in range(18):
+for x in range(int(54/members)):
   random.seed()
   random.shuffle(all)
   mycards.append(all.pop())
