d9e7952f7c5a4efa7f1306c5343b5d51302eb6e1
Sato Makoto <makoto@kuzuore.com>
2021-01-28 13:15:45 +0900

    celsius fahrenheit

---
+++ python3/cel_hur.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+print("c h")
+for c in range(-100,300):
+  h = round((9/5) * c + 32, 1)
+  print(c,h)
---
+++ python3/hur_cel.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+print("h c")
+for h in range(-100,300):
+  c = round(5/9 * (h - 32), 1)
+  print(h,c)
