cd99b7cd6d800210fcf56bd0259fd2cb128e608f
Sato Makoto <makoto@kuzuore.com>
2021-07-30 21:43:45 +0900

    add lan and wan ping graph

---
+++ ping/two.r
@@ -0,0 +1,32 @@
+#!/usr/bin/env Rscript
+
+args = commandArgs(trailingOnly=TRUE)
+
+wan=read.table("wan", header=T)
+lan=read.table("lan", header=T)
+png(filename=paste("ping_wan_and_lan",".png",sep=""), width=1024,height=768)
+par(ps=16,oma=c(2,2,2,2))
+# par(family="IPAMincho",ps=20,oma=c(2,2,2,2))
+# oma=c(under, left, upper, right)
+# mar: inline
+plot    (
+        lan$Time, lan$MS,
+        type="l", 
+	col="3",
+#	log="y",
+	ylim=c(0,250),
+        xlab="ping time",
+        ylab="ms",
+        main="wireless ping -c 100 wan and router"
+        )
+points	(
+        wan$Time, wan$MS,
+	type="l",
+	col="4",
+)
+legend	("topright", c("ping wan host", "ping wifi router"),
+	pch = 15,
+	col = c(4,3)
+)
+dev.off()
+
