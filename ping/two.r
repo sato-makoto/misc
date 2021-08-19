c4b4cdaa1a4aa0de95289fa4bc5ede448d66319c
Sato Makoto <makoto@kuzuore.com>
2021-08-19 16:16:18 +0900

    add total.r changes to two.r

--- ping/two.r
+++ ping/two.r
@@ -4,6 +4,7 @@ args = commandArgs(trailingOnly=TRUE)
 
 wan=read.table("wandata", header=T)
 lan=read.table("landata", header=T)
+zero=read.table("zerodata", header=T)
 png(filename=paste("ping_wan_and_lan",".png",sep=""), width=1024,height=768)
 par(ps=16,oma=c(2,2,2,2))
 # par(family="IPAMincho",ps=20,oma=c(2,2,2,2))
@@ -16,13 +17,18 @@ plot    (
 #	log="y",
 	ylim=c(0,250),
         xlab="ping time",
-        ylab="ms",
+        ylab="ms(0 means packet loss)",
         main="wireless ping6 -c 100 wan and router"
         )
 points	(
         wan$Time, wan$MS,
 	type="l",
 	col="4",
+)
+points	(
+        zero$Time, zero$MS,
+	type="l",
+	col="2",
 )
 legend	("topright", c("ping wan host", "ping wifi router"),
 	pch = 15,
