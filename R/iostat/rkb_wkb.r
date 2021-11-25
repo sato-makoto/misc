8b4adc9f104331e748f34741db0f246ed84b8721
Sato Makoto <makoto@kuzuore.com>
2021-11-25 16:09:08 +0900

    change graph plot

--- R/iostat/rkb_wkb.r
+++ R/iostat/rkb_wkb.r
@@ -5,14 +5,14 @@ wkB_s=read.table("sdb_wkB_s", header=T)
 png(filename="sda_r_sdb_w.png", width=1024,height=768)
 #pdf(file="test.pdf")
 plot(
-	rkB_s$Time, rkB_s$sda_rkB_s, type="l", col="3",
+	wkB_s$Time, wkB_s$sdb_wkB_s, type="l", col="2",
         xlab="時間(秒)",
         ylab="rkB or wkB(s)",
 #	ylim=c(-5,105),
         main="ddrescue iostat"
 )
 points(
-	wkB_s$Time, wkB_s$sdb_wkB_s, type="l", col="2",
+	rkB_s$Time, rkB_s$sda_rkB_s, type="l", col="3",
 )
 legend("topright",c("sda: rkB/s", "sdb: wkB/s"),
 		pch = 15,
--- R/iostat/util.r
+++ R/iostat/util.r
@@ -5,14 +5,14 @@ util_b=read.table("sdb_util", header=T)
 png(filename="util.png", width=1024,height=768)
 #pdf(file="test.pdf")
 plot(
-	util_a$Time, util_a$sda_util, type="l", col="3",
+	util_b$Time, util_b$sdb_util, type="l", col="2",
         xlab="時間(秒)",
         ylab="util(%)",
 #	ylim=c(-5,105),
         main="ddrescue iostat util"
 )
 points(
-	util_b$Time, util_b$sdb_util, type="l", col="2",
+	util_a$Time, util_a$sda_util, type="l", col="3",
 )
 legend("topright",c("sda util(%)", "sdb util(%)"),
 		pch = 15,
