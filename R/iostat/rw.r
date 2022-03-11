a444ee4de39c62dc0af2ae5ecccf6e41e6904869
Sato Makoto <makoto@kuzuore.com>
2022-03-11 14:44:41 +0900

    chage read/write

--- R/iostat/rw.r
+++ R/iostat/rw.r
@@ -5,14 +5,14 @@ ws=read.table("sdb_ws", header=T)
 png(filename="sda_rs_sdb_ws.png", width=1024,height=768)
 #pdf(file="test.pdf")
 plot(
-	ws$Time, ws$sdb_ws, type="l", col="2",
+	rs$Time, rs$sda_rs, type="l", col="3",
         xlab="時間(秒)",
         ylab="r/s and w/s",
 #	ylim=c(-5,105),
         main="ddrescue iostat"
 )
 points(
-	rs$Time, rs$sda_rs, type="l", col="3",
+	ws$Time, ws$sdb_ws, type="l", col="2",
 )
 legend("topright",c("sda: r/s", "sdb: w/s"),
 		pch = 15,
--- R/iostat/rwreq.r
+++ R/iostat/rwreq.r
@@ -5,14 +5,14 @@ wareq=read.table("sdb_wareq", header=T)
 png(filename="sda_rareq_sdb_wareq.png", width=1024,height=768)
 #pdf(file="test.pdf")
 plot(
-	wareq$Time, wareq$sdb_wareq, type="l", col="2",
+	rareq$Time, rareq$sda_rareq, type="l", col="3",
         xlab="時間(秒)",
         ylab="rareq or wareq",
 #	ylim=c(-5,105),
         main="ddrescue iostat"
 )
 points(
-	rareq$Time, rareq$sda_rareq, type="l", col="3",
+	wareq$Time, wareq$sdb_wareq, type="l", col="2",
 )
 legend("topright",c("sda: rareq-sz", "sdb: wareq-sz"),
 		pch = 15,
