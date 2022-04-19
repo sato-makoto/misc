66c62d07627656c2d38ef34f872237937adfac72
Sato Makoto <makoto@kuzuore.com>
2022-04-19 13:22:44 +0900

    bug fix get.sh (real)

--- R/iostat/get.sh
+++ R/iostat/get.sh
@@ -3,11 +3,15 @@
 # Time and predict remaining time data: second
 # Time and real remaining time data : real
 
+if [ -e real ]; then
+	rm real
+fi
+
 awk -f get.awk typescript > second
 end=`awk 'END{print $1}' second`
 for time in `cut -f 2 -d " " second | tac`
 do
-	echo `expr $end - $time` $time >> real
+	echo $time `expr $end - $time` >> real
 done
 
 sed -i '1iTime Second' second
