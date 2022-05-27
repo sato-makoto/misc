206bce1ae7a0de52eaaa8b8195c400b4cc1c5585
Sato Makoto <makoto@kuzuore.com>
2022-05-27 17:34:04 +0900

    add date to ping_png

--- ping/ping_png.r
+++ ping/ping_png.r
@@ -3,6 +3,7 @@ args = commandArgs(trailingOnly=TRUE)
 
 mydata=read.table(args, header=T)
 png(filename=paste(args,".png",sep=""), width=800,height=600)
+date = paste("作成日時: ", Sys.time())
 par(ps=16,oma=c(2,2,2,2))
 # par(family="IPAMincho",ps=20,oma=c(2,2,2,2))
 # oma=c(under, left, upper, right)
@@ -16,5 +17,7 @@ plot    (
         ylab="ms(log)",
         main="ping 8.8.8.8 in 地下鉄谷町線 中崎町-東梅田間(対数表示)"
         )
+mtext(text=date,adj = 0)
+warnings()
 dev.off()
 
